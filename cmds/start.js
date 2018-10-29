5c16bb34762525f66adf44de38827dc4de4eef49
Victor Farfan <vfarfan@ufm.edu>
2018-10-28 18:25:14 -0600

    Command "start" displays the Tic-Tac-Toe board

---
+++ cmds/start.js
@@ -0,0 +1,19 @@
+tl = " ";
+tc = " ";
+tr = " ";
+cl = " ";
+cc = " ";
+cr = " ";
+bl = " ";
+bc = " ";
+br = " ";
+
+module.exports = (args) => {
+    console.log();
+    console.log(" " + tl + " | " + tc + " | " + tr + " ");
+    console.log("-----------")
+    console.log(" " + cl + " | " + cc + " | " + cr + " ");
+    console.log("-----------")
+    console.log(" " + bl + " | " + bc + " | " + br + " ");
+}
+
